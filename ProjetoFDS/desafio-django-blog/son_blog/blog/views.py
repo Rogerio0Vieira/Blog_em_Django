560c6cf9e75528f97367eb19aa88ddcc3d7109cf
Rogério Vieira <43701289+Rogerio0Vieira@users.noreply.github.com>
2021-01-30 14:16:32 -0300

    Quase finalizado
    Faltam algumas lapidações

--- ProjetoFDS/desafio-django-blog/son_blog/blog/views.py
+++ ProjetoFDS/desafio-django-blog/son_blog/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    return render(request, "home.html", {'post': posts})
+
+
+def post(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'post.html', {'post': post})
--- ProjetoFDS/desafio-django-blog/son_blog/son_blog/urls.py
+++ ProjetoFDS/desafio-django-blog/son_blog/son_blog/urls.py
@@ -19,9 +19,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls.conf import include
+from blog import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', views.home),
+    path('post/<int:post_id>', views.post)
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
